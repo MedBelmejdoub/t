cd94f46d49e7eca712ce82ec741354a634dc6bb1
Med <BelmejdoubMed@gmail.com>
2021-09-11 14:44:12 +0100

    Add Nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Nav.css';
+export const Nav = () => {
+  return (
+    <nav className='nav'>
+      <div>Home</div>
+
+      <div>Stock </div>
+
+      <div>Admin </div>
+
+      <div>About </div>
+    </nav>
+  );
+};
