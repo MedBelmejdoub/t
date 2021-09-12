3c6618564f3f8e838048cdd24f078f3e590a0f34
Med <BelmejdoubMed@gmail.com>
2021-09-12 23:10:04 +0100

    Adding Nav and forms Items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-
 import "./App.css";
 import { Nav } from "./Nav/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home } from "./Home/Home";
+import { Stock } from "./Stock/Stock";
+import { Admin } from "./Admin/Admin";
+// i will Import About Later
 function App() {
+  const date = new Date().toISOString();
+
   return (
-    <form>
-      <div className="App">
-        <header className="App-header">
-          <Nav />
-        </header>
+    <div className="nav-app">
+      <div>
+        <Router>
+          <nav>
+            <div>
+              <Nav />
+              <Home />
+              <Stock />
+              <Admin />
+              <Route path="/" component={Home} />
+              <Route path="/Stock" component={Stock} />
+              <Route path="/Admin" component={Admin} />
+            </div>
+          </nav>
+        </Router>
       </div>
-    </form>
+      <footer>{<time datetime={date}></time>}</footer>
+    </div>
   );
 }
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,15 +1,24 @@
 import React from "react";
-import './Nav.css';
+import "./Nav.css";
+import { Link } from "react-router-dom";
 export const Nav = () => {
   return (
-    <nav className='nav'>
-      <div>Home</div>
-
-      <div>Stock </div>
-
-      <div>Admin </div>
-
-      <div>About </div>
+    <nav className="nav-bar">
+      <h3 className="nav-logo">LOGO</h3>
+      <ul className="nav-list">
+        <Link  className="nav-links" to="/">
+          <li>Home</li>
+        </Link>
+        <Link  className="nav-links" to="/Stock">
+          <li>Stock</li>
+        </Link>
+        <Link  className="nav-links" to="/Admin">
+          <li>Admin</li>
+        </Link>
+        <Link  className="nav-links" to="/About">
+          <li>About</li>
+        </Link>
+      </ul>
     </nav>
   );
 };
